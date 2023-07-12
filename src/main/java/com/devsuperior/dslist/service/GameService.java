d42a7c9f18c5b076c628264c74d3d0372afaf6bf
Danilo Cunha <danilowlc@gmail.com>
2023-07-11 22:06:55 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -20,11 +20,14 @@ public class GameService {
 	
 	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
+		
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
 		return dto;
+		
 	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
